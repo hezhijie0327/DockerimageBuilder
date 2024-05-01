faf2d74db3f42231920f3fd13fa4a54d77c5065f
Zhijie He <hezhijie0327@hotmail.com>
2024-05-01 16:52:44 +0800

    intel.sh | Version 1.0.202405011652

--- patch/jellyfin/intel.sh
+++ patch/jellyfin/intel.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-GHPROXY_URL=""
-DOWNLOAD_DIR="/tmp"
-
 which "curl" > "/dev/null" 2>&1
 if [ "$?" -eq "1" ]; then
     apt-get update && apt-get install -qy curl
@@ -16,12 +13,12 @@ fi
 for i in $(cat "/opt/intel-patch/intel.version" | awk "{print $2}"); do
     if [ -n "$GHPROXY_URL" ]; then
         i=$(echo $i | sed "s|https://github.com|https://${GHPROXY_URL}/https://github.com|g")
-    fi && wget -P $DOWNLOAD_DIR $i
+    fi && wget -P ${DOWNLOAD_DIR:-/tmp} $i
 done
 
-if [ -f $DOWNLOAD_DIR/*.deb ]; then
-    dpkg -i $DOWNLOAD_DIR/*.deb && rm -rf $DOWNLOAD_DIR/*.deb
+if [ -f ${DOWNLOAD_DIR:-/tmp}/*.deb ]; then
+    dpkg -i ${DOWNLOAD_DIR:-/tmp}/*.deb && rm -rf ${DOWNLOAD_DIR:-/tmp}/*.deb
 fi
-if [ -f $DOWNLOAD_DIR/*.ddeb ]; then
-    dpkg -i $DOWNLOAD_DIR/*.ddeb && rm -rf $DOWNLOAD_DIR/*.ddeb
+if [ -f ${DOWNLOAD_DIR:-/tmp}/*.ddeb ]; then
+    dpkg -i ${DOWNLOAD_DIR:-/tmp}/*.ddeb && rm -rf ${DOWNLOAD_DIR:-/tmp}/*.ddeb
 fi
