edc2a85e971168cb3b9faa308e1440c98ce2cdd6
Zhijie He <hezhijie0327@hotmail.com>
2024-07-12 13:54:22 +0800

    lobechat | Version 1.0.202407121354

---
+++ patch/lobechat/webrtc/webrtc.js
@@ -0,0 +1,199 @@
+// WebRTC Signaling server for LobeChat
+// ref: https://github.com/lobehub/y-webrtc-signaling
+
+import { WebSocketServer } from 'ws'
+import http from 'http'
+import * as map from 'lib0/map'
+
+// Environment variables for port and host
+const host = process.env.WEBRTC_HOST || '0.0.0.0'
+const port = process.env.WEBRTC_PORT || 3001
+
+// Create an HTTP server
+const server = http.createServer( ( request, response ) =>
+{
+    response.writeHead( 200, { 'Content-Type': 'text/plain' } )
+    response.end( 'okay' )
+} )
+
+// Create a new WebSocket server
+const wss = new WebSocketServer( { noServer: true } )
+
+// Ping timeout interval in milliseconds
+const pingTimeout = 30000
+
+// WebSocket ready states
+const wsReadyState = [
+    0, // CONNECTING
+    1, // OPEN
+    2, // CLOSING
+    3 // CLOSED
+]
+
+/**
+ * Map from topic-name to set of subscribed clients.
+ * @type {Map<string, Set<any>>}
+ */
+const topics = new Map()
+
+/**
+ * Send a message to a WebSocket connection
+ * @param {any} conn - WebSocket connection
+ * @param {object} message - Message to send
+ */
+const send = ( conn, message ) =>
+{
+    if ( conn.readyState !== wsReadyState[ 0 ] && conn.readyState !== wsReadyState[ 1 ] )
+    {
+        conn.close()
+    }
+    try
+    {
+        conn.send( JSON.stringify( message ) )
+    } catch ( e )
+    {
+        conn.close()
+    }
+}
+
+/**
+ * Setup a new client connection
+ * @param {any} conn - WebSocket connection
+ */
+const onconnection = conn =>
+{
+    /**
+     * Set of topics subscribed by the client
+     * @type {Set<string>}
+     */
+    const subscribedTopics = new Set()
+    let closed = false
+
+    // Check if connection is still alive
+    let pongReceived = true
+    const pingInterval = setInterval( () =>
+    {
+        if ( !pongReceived )
+        {
+            conn.close()
+            clearInterval( pingInterval )
+        } else
+        {
+            pongReceived = false
+            try
+            {
+                conn.ping()
+            } catch ( e )
+            {
+                conn.close()
+            }
+        }
+    }, pingTimeout )
+
+    // Listen for pong responses to keep the connection alive
+    conn.on( 'pong', () =>
+    {
+        pongReceived = true
+    } )
+
+    // Handle connection close event
+    conn.on( 'close', () =>
+    {
+        subscribedTopics.forEach( topicName =>
+        {
+            const subs = topics.get( topicName ) || new Set()
+            subs.delete( conn )
+            if ( subs.size === 0 )
+            {
+                topics.delete( topicName )
+            }
+        } )
+        subscribedTopics.clear()
+        closed = true
+    } )
+
+    /**
+     * Handle incoming messages from the client
+     * @param {object} message - Incoming message
+     */
+    conn.on( 'message', message =>
+    {
+        if ( typeof message === 'string' || message instanceof Buffer )
+        {
+            message = JSON.parse( message )
+        }
+        if ( message && message.type && !closed )
+        {
+            switch ( message.type )
+            {
+                case 'subscribe':
+                    /**
+                     * @type {Array<string>}
+                     */
+                    ( message.topics || [] ).forEach( topicName =>
+                    {
+                        if ( typeof topicName === 'string' )
+                        {
+                            // Add connection to the topic
+                            const topic = map.setIfUndefined( topics, topicName, () => new Set() )
+                            topic.add( conn )
+                            // Add topic to the connection's subscribed topics
+                            subscribedTopics.add( topicName )
+                        }
+                    } )
+                    break
+                case 'unsubscribe':
+                    /**
+                     * @type {Array<string>}
+                     */
+                    ( message.topics || [] ).forEach( topicName =>
+                    {
+                        const subs = topics.get( topicName )
+                        if ( subs )
+                        {
+                            subs.delete( conn )
+                        }
+                    } )
+                    break
+                case 'publish':
+                    if ( message.topic )
+                    {
+                        const receivers = topics.get( message.topic )
+                        if ( receivers )
+                        {
+                            message.clients = receivers.size
+                            receivers.forEach( receiver =>
+                                send( receiver, message )
+                            )
+                        }
+                    }
+                    break
+                case 'ping':
+                    send( conn, { type: 'pong' } )
+            }
+        }
+    } )
+}
+
+// Set up WebSocket connection handler
+wss.on( 'connection', onconnection )
+
+// Handle HTTP upgrade requests to WebSocket
+server.on( 'upgrade', ( request, socket, head ) =>
+{
+    /**
+     * You may check auth of request here..
+     * @param {any} ws
+     */
+    const handleAuth = ws =>
+    {
+        wss.emit( 'connection', ws, request )
+    }
+    wss.handleUpgrade( request, socket, head, handleAuth )
+} )
+
+// Start the HTTP server and bind it to the specified host and port
+server.listen( port, host, () =>
+{
+    console.log( `WebRTC Signaling server running on ${ host }:${ port }` )
+} )
