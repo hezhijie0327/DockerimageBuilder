5ee08c75ab9759537fe7bc7163e74c36d5d48d49
Zhijie He <hezhijie0327@hotmail.com>
2025-08-23 17:24:58 +0800

    release.sh | Version 2.3.2

--- patch/release.sh
+++ patch/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 2.3.1
+# Current Version: 2.3.2
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/DockerimageBuilder.git" && bash ./DockerimageBuilder/patch/release.sh
@@ -8,7 +8,6 @@
 ## Parameter
 export BROWSERLESS_VERSION_FIXED=""
 export CLOUDFLARED_VERSION_FIXED=""
-export CLOUDREVE_VERSION_FIXED=""
 export ICU_VERSION_FIXED=""
 export JELLYFIN_VERSION_FIXED=""
 export JELLYFIN_WEB_VERSION_FIXED=""
@@ -37,7 +36,6 @@ export XRAY_VERSION_FIXED=""
 function GetLatestVersion() {
     BROWSERLESS_VERSION=$(curl -s --connect-timeout 15 "https://api.github.com/repos/browserless/browserless/git/matching-refs/tags" | jq -Sr ".[].ref" | grep "^refs/tags/v" | grep -v "-" | tail -n 1 | sed "s/refs\/tags\/v//")
     CLOUDFLARED_VERSION=$(curl -s --connect-timeout 15 "https://api.github.com/repos/cloudflare/cloudflared/git/matching-refs/tags" | jq -Sr ".[].ref" | grep "^refs/tags/" | tail -n 1 | sed "s/refs\/tags\///")
-    CLOUDREVE_VERSION=$(curl -s --connect-timeout 15 "https://api.github.com/repos/cloudreve/cloudreve/git/matching-refs/tags" | jq -Sr ".[].ref" | grep "^refs/tags/" | grep -v "alpha\|beta\|rc" | tail -n 1 | sed "s/refs\/tags\///")
     ICU_VERSION=$(curl -s --connect-timeout 15 "https://api.github.com/repos/unicode-org/icu/git/matching-refs/tags" | jq -Sr ".[].ref" | grep "^refs/tags/release" | grep -v "alpha\|eclipse\|rc\|preview" | tail -n 1 | sed "s/refs\/tags\/release\-//")
     JELLYFIN_VERSION=$(curl -s --connect-timeout 15 "https://api.github.com/repos/jellyfin/jellyfin/git/matching-refs/tags" | jq -Sr ".[].ref" | grep "^refs/tags/v" | grep -v "-" | tail -n 1 | sed "s/refs\/tags\/v//")
     JELLYFIN_WEB_VERSION=$(curl -s --connect-timeout 15 "https://api.github.com/repos/jellyfin/jellyfin-web/git/matching-refs/tags" | jq -Sr ".[].ref" | grep "^refs/tags/v" | grep -v "-" | tail -n 1 | sed "s/refs\/tags\/v//")
@@ -66,7 +64,6 @@ function GenerateReplacements() {
     replacement_list=(
         "s/{BROWSERLESS_VERSION}/${BROWSERLESS_VERSION_FIXED:-${BROWSERLESS_VERSION}}/g"
         "s/{CLOUDFLARED_VERSION}/${CLOUDFLARED_VERSION_FIXED:-${CLOUDFLARED_VERSION}}/g"
-        "s/{CLOUDREVE_VERSION}/${CLOUDREVE_VERSION_FIXED:-${CLOUDREVE_VERSION}}/g"
         "s/{ICU_VERSION}/${ICU_VERSION_FIXED:-${ICU_VERSION}}/g"
         "s/{ICU_VERSION_}/$(echo ${ICU_VERSION_FIXED:-${ICU_VERSION}} | tr '-' '_')/g"
         "s/{JELLYFIN_VERSION}/${JELLYFIN_VERSION_FIXED:-${JELLYFIN_VERSION}}/g"
